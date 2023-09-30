e6b32db409e7617e5c4615c2d8620ee51ed20c6d
NebovarenkovStepan <nebovarenkovstipan@gmail.com>
2023-09-30 11:45:36 +0300

    first task

--- 3.2/3.2.c
+++ 3.2/3.2.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include<math.h>
 int c;
-
+/**
+ * \brief formula for calculating factorial
+ * \param parameter m - the factorial number of which must be calculated
+ * \return factorial
+ */
 int factorial(int m) {
     c = 1;
     for (int i = 1; i <= m; i++) {
