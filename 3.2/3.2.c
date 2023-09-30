270d387114694ce7646a05d8cbd10852567ddeaf
NebovarenkovStepan <nebovarenkovstipan@gmail.com>
2023-09-30 12:10:20 +0300

    third.two task

--- 3.2/3.2.c
+++ 3.2/3.2.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 #include<math.h>
-int c;
+#include <stdlib.h>
+double c;
 /**
  * \brief formula for calculating factorial
  * \param parameter m - the factorial number of which must be calculated
- * \return factorial 
+ * \return factorial
  */
-int factorial(int m);
+double factorial(double m);
+/**
+ * \brief Function that calculates the possibility of input.
+ * \param res is the number of arguments.
+ */
+void number_of_arguments(int res);
 int main()
 {
     double n, answ, e;
-    printf("%s\n", "Enter n: ");
-    scanf("%lf", &n);
-    printf("%s\n", "Enter e: ");
-    scanf("%lf", &e);
+    answ = 0;
+    printf("%s", "Enter n: ");
+    int res1 = scanf_s("%lf", &n);
+    number_of_arguments(res1);
+    printf("%s", "Enter e: ");
+    int res2 = scanf_s("%lf", &e);
+    number_of_arguments(res2);
     for (int i = 1; i <= n; i++) {
         answ = answ + (pow(-1, i) * (1 / (factorial(i) * factorial(i + 1))));
     }
@@ -24,11 +33,19 @@ int main()
     } while (e <= n);
     return 0;
 }
-int factorial(int m)
+double factorial(double m)
 {
     c = 1;
     for (int i = 1; i <= m; i++) {
         c = c * i;
     }
     return c;
+}
+void number_of_arguments(int res)
+{
+    if (res != 1)
+    {
+        puts("Error!");
+        abort();
+    }
 }
