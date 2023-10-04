0742a43f6cf68a08f553c8d52e7ad8c9207b245e
NebovarenkovStepan <nebovarenkovstipan@gmail.com>
2023-10-04 22:07:41 +0300

    one.one true rework)))

--- 1.1/1.1.c
+++ 1.1/1.1.c
@@ -5,29 +5,36 @@
  * \param x is x.
  * \param y is y.
  * \param z is z.
+ * \return a.
  */
-void get_a(float x, float y, float z);
+double get_a(float x, float y, float z);
 /**
  * \brief Function calculating and outputting b
  * \param x is x.
  * \param y is y.
  * \param z is z.
+ * \return b.
  */
-void get_b(float x, float y, float z);
+double get_b(float x, float y, float z);
 int main()
 {
-    get_a(1.4, 3.1, 0.5); // a=0.870404
-    get_b(1.4, 3.1, 0.5); // b=-50.893158
+    const double x = 1.4;
+    const double y = 3.1;
+    const double z = 0.5;
+    double a = get_a(x, y, z); // a=0.870404
+    double b = get_b(x, y, z); // b=-50.893158
+    printf("a= %f\n", a);
+    printf("b= %f\n", b);
     return 0;
 }
-void get_a(float x, float y, float z)
+double get_a(float x, float y, float z)
 {
     float a = sqrt(z * x * sin(2 * x) + sqrt(expl(-2 * x) * (x + y)));
     float c = expl(-2 * x);
-    printf("a= %f\n", a);
+    return a;
 }
-void get_b(float x, float y, float z)
+double get_b(float x, float y, float z)
 {
     float b = expl(2 * x) * log(z + x) - pow(y, 3 * x) * log(y - x);
-    printf("b= %f\n", b);
+    return b;
 }
