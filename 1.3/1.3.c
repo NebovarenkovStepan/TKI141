e134fd40ea6c4b299337562da409b4112aa1d8df
NebovarenkovStepan <nebovarenkovstipan@gmail.com>
2023-10-04 22:01:37 +0300

    one.three rework

--- 1.3/1.3.c
+++ 1.3/1.3.c
@@ -9,8 +9,9 @@ void number_of_arguments(int res);
 /**
  * \brief Function that calculates the possibility of input.
  * \param res is the number of arguments.
+ * \return pressure
  */
-void get_pressure(const double q, const double g, double h);
+double get_pressure(const double q, const double g, double h);
 int main()
 {
 	double h;
@@ -20,14 +21,14 @@ int main()
 	printf("%s", "Enter h: ");
 	res = scanf_s("%lf", &h);
 	number_of_arguments(res);
-	get_pressure(q, g, h);
+	puts("Bottom pressure: ");
+	printf("P=%lf\n", get_pressure(q, g, h));
 	return 0;
 }
-void get_pressure(double q, double g, double h)
+double get_pressure(double q, double g, double h)
 {
-	double P = q * g * h;
-	printf("%s", "Bottom pressure: ");
-	printf("P=%lf\n", P);
+	double pressure = q * g * h;
+	return pressure;
 }
 void number_of_arguments(int res)
 {
