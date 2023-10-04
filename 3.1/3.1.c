b011f9a19248b01a67ef43b2a9d2339309e491c1
NebovarenkovStepan <nebovarenkovstipan@gmail.com>
2023-10-04 22:03:49 +0300

    three.one rework

--- 3.1/3.1.c
+++ 3.1/3.1.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
 /**
  * \brief A function that calculates the possibility of evaluating an expression for a given value of x.
  * \param x is the argument.
  * \return Return 1 - true and 0 - false
  */
-float answer_possibility(float x);
+bool answer_possibility(float x);
 /**
  * \brief Function that calculates the possibility of input.
  * \param res is the number of arguments.
@@ -20,19 +21,20 @@ int main()
     res = scanf_s("%f", &x);
     number_of_arguments(res);
     puts("Enter dx: ");
-    scanf_s("%f", &dx);
-    number_of_arguments(res);
+    int res1 = scanf_s("%f", &dx);
+    number_of_arguments(res1);
     do {
-        if (answer_possibility(x) == 0) printf("x=%f\n", x);
-        if (answer_possibility(x) == 0) printf("%s\n", "No answer");
+        if (answer_possibility(x) == 0) {
+            printf("x=%f\n", x);
+            printf("%s\n", "No answer");
+        }
         x = x + dx;
     } while (x <= 3);
     return 0;
 }
-float answer_possibility(float x)
+bool answer_possibility(float x)
 {
-    if ((3 * x) - 14 + exp(x) - exp(-x)) return 1;
-    else return 0;
+    return ((3 * x) - 14 + exp(x) - exp(-x));
 }
 void number_of_arguments(int res)
 {
