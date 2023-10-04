bed3f8428fc381a9bb4fea812e15e6827af43ff5
NebovarenkovStepan <nebovarenkovstipan@gmail.com>
2023-10-04 22:02:14 +0300

    two.two rework

--- 2.2/2.2.c
+++ 2.2/2.2.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <float.h>
+#include <stdbool.h>
 /**
  * \brief Function that calculates the possibility of input.
  * \param res is the number of arguments.
@@ -10,25 +12,27 @@ void number_of_arguments(int res);
  * \brief Function that calculates y in the first case.
  * \param x is the x parameter.
  * \param a is the a parameter.
+ * \return y.
  */
-void get_y_1(double x, double a);
+double get_y_1(double x, double a);
 /**
  * \brief Function that calculates y in the second case.
  * \param x is the x parameter.
  * \param a is the a parameter.
+ * \return y.
  */
-void get_y_2(double x, double a);
+double get_y_2(double x, double a);
 int main()
 {
-    double x, a;
-    a = 1.6;
+    double x;
+    const double a = 1.6;
     puts("Enter x: ");
     int res = scanf_s("%lf", &x);
     printf("res=%d\n", res);
     number_of_arguments(res);
-    if (x > a) get_y_1(x, a);
-    if (x == 0) printf("%s\n", "y = 3");
-    if (x < a) if (x != 0) get_y_2(x, a);
+    if (x > a) printf("y=%lf\n", get_y_1(x, a));
+    else if (x < a && x != 0) printf("y=%lf\n", get_y_2(x, a));
+    else printf("%s\n", "y = 3");
     return 0;
 }
 void number_of_arguments(int res)
@@ -39,13 +43,13 @@ void number_of_arguments(int res)
         abort();
     }
 }
-void get_y_1(double x, double a)
+double get_y_1(double x, double a)
 {
     double y = exp(a * x) - 2.5;
-    printf("y=%lf\n", y);
+    return y;
 }
-void get_y_2(double x, double a)
+double get_y_2(double x, double a)
 {
     double y = a / x + sin(x);
-    printf("y=%lf\n", y);
+    return y;
 }
