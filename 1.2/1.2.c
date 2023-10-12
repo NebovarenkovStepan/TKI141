0094ec41049122a2c7d63e90564365c4edf38be1
NebovarenkovStepan <nebovarenkovstipan@gmail.com>
2023-10-12 22:33:07 +0300

    1.2 update

--- 1.2/1.2.c
+++ 1.2/1.2.c
@@ -8,18 +8,21 @@
  * \param res количесво аргументов.
  */
 void number_of_arguments(int res);
+
 /**
  * @brief Функция расчитывающая площадь круга.
  * \param L длинна круга.
  * \return Площадь.
  */
 double get_area(double L);
+
 /**
   * @brief Функция расчитывающая площадь круга.
  * \param L длинна круга.
  * \return Условное значение, true если L > 0 и false если L <=0.
  */
 bool right_numb(double L);
+
 int main()
 {
     double L;
@@ -33,6 +36,7 @@ int main()
     else puts("Error!");
     return 0;
 }
+
 void number_of_arguments(int res)
 {
     if (res != 1)
@@ -41,11 +45,13 @@ void number_of_arguments(int res)
         abort();
     }
 }
+
 double get_area(double L)
 {
     double S = ((pow(L, 2) / (4 * M_PI)));
     return S;
 }
+
 bool right_numb(double L)
 {
     return L > 0;
