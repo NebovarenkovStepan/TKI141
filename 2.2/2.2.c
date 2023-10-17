7f4cce0bbb6c16d5e9ca05d91b76f5e9a235f3ac
NebovarenkovStepan <nebovarenkovstipan@gmail.com>
2023-10-17 19:34:55 +0300

    2.2 improve

--- 2.2/2.2.c
+++ 2.2/2.2.c
@@ -1,58 +1,69 @@
 ﻿#include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#include <float.h>
-/**
- * @brief Функция расчитывающая возможность ввода.
- * \param res количество аргументов.
- */
-void number_of_arguments(int res);
+#include <errno.h>
+
 /**
  * @brief Функция расчитывающая 1-ый случай.
- * \param x это x.
- * \param a это a.
- * \return y.
+ * @param x это x.
+ * @param a это a.
+ * @return y.
  */
 double get_y_1(double x, double a);
+
 /**
  * @brief Функция расчитывающая 2-ой случай.
- * \param x это x.
- * \param a это a.
- * \return y.
+ * @param x это x.
+ * @param a это a.
+ * @return y.
  */
 double get_y_2(double x, double a);
+
+/**
+ * @brief Функция принимающая и проверяющая значение на ввод.
+ * @return Значение.
+ */
+double get_value();
+
 int main()
 {
     double x;
     const double a = 1.6;
     puts("Enter x: ");
-    int res = scanf_s("%lf", &x);
-    number_of_arguments(res);
+    x = get_value();
     if (x > a) printf("y=%lf\n", get_y_1(x, a));
-    else if (x < a && x != 0) printf("y=%lf\n", get_y_2(x, a));
     else if (x == 0) printf("%s\n", "y = 3");
+    else if (x < a) printf("y=%lf\n", get_y_2(x, a));
     else
     {
-        puts("Error!");
-        abort();
+        errno = EIO;
+        perror("Wrong value");
+        return 1;
     }
     return 0;
 }
-void number_of_arguments(int res)
-{
-    if (res != 1)
-    {
-        puts("Error!");
-        abort();
-    }
-}
+
 double get_y_1(double x, double a)
 {
     double y = exp(a * x) - 2.5;
     return y;
 }
+
 double get_y_2(double x, double a)
 {
     double y = a / x + sin(x);
     return y;
+}
+
+double get_value()
+{
+    double a;
+    int res = scanf_s("%lf", &a);
+    if (res != 1)
+    {
+        errno = EIO;
+        perror("Wrong value");
+        abort();
+    }
+    return a;
 }
