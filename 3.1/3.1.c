d8de1b91312a60aa034e1d9a527b7ba7675f5b36
NebovarenkovStepan <nebovarenkovstipan@gmail.com>
2023-10-17 19:47:44 +0300

    3.1 без блок схемы

---
+++ 3.1/3.1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+#include <errno.h>
+
+/**
+ * @brief Функция проеряющая существование функции в этой точке.
+ * @param x сама точка.
+ * @return Возвращает 1 - true или 0 - false
+ */
+bool answer_possibility(float x);
+
+/**
+ * @brief Функция принимающая и проверяющая значение на ввод.
+ * @return Значение.
+ */
+double get_value();
+
+int main()
+{
+    int res;
+    float x, dx;
+    puts("Enter x: ");
+    x = get_value();
+    puts("Enter dx: ");
+    dx = get_value();
+    do {
+        if (answer_possibility(x) == 0) {
+            printf("x=%f\n", x);
+            printf("%s\n", "No answer");
+        }
+        x = x + dx;
+    } while (x <= 3);
+    return 0;
+}
+
+bool answer_possibility(float x)
+{
+    return ((3 * x) - 14 + exp(x) - exp(-x));
+}
+
+double get_value()
+{
+    double a;
+    int res = scanf_s("%lf", &a);
+    if (res != 1)
+    {
+        errno = EIO;
+        perror("Wrong value");
+        abort();
+    }
+    return a;
+}
