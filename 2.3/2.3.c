06203b6b3854ab33aacf5980394e6d212f6fe558
NebovarenkovStepan <nebovarenkovstipan@gmail.com>
2023-10-24 21:32:56 +0300

    2.3 исправление

--- 2.3/2.3.c
+++ 2.3/2.3.c
@@ -2,43 +2,58 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdbool.h>
+#include <errno.h>
+
 /**
- * @brief Функция показывает возможность вписания двух прямоугольников в круг.
- * \param a первая сторона первого прямоугольника.
- * \param b вторая сторона первого прямоугольника.
- * \param c первая сторона второго прямоугольника.
- * \param d вторая сторона второго прямоугольника.
- * \param r это радиус.
- * \return возможность.
- */
+* @brief Функция показывает возможность вписания двух прямоугольников в круг.
+* \param a первая сторона первого прямоугольника.
+* \param b вторая сторона первого прямоугольника.
+* \param c первая сторона второго прямоугольника.
+* \param d вторая сторона второго прямоугольника.
+* \param r это радиус.
+* \return возможность.
+*/
 bool possibility(double a, double b, double c, double d, double r);
 
 /**
- * @brief Функция проеряющая количество аргументов(для 2-ух).
- * \param res количество аргументов.
- */
-void number_of_arguments_for_two(int res);
+* @brief Функция принимающая и проверяющая значение на ввод.
+* @return Значение.
+*/
+double get_value();
 
 /**
- * @brief Функция проеряющая количество аргументов(для 1-ого).
- * \param res количество аргументов.
- */
-void number_of_arguments_for_one(int res);
+* @brief Функция получения максимально возможной высоты в круге.
+* @param r - Радиус.
+* @param side - Значение стороны.
+* @return Значение высоты.
+*/
+double get_h(double r, double side);
 
+/**
+* @brief Основная функция в программе.
+* @return 0 если процесс завершился без ошибок.
+*/
 int main()
 {
 	double r, a, b, c, d;
 	puts("Enter r: ");
-	int res1 = scanf_s("%lf", &r);
-	number_of_arguments_for_one(res1);
-	puts("Enter a, b: ");
-	int res2 = scanf_s("%lf%lf", &a, &b);
-	number_of_arguments_for_two(res2);
-	puts("Enter c, d: ");
-	int res3 = scanf_s("%lf%lf", &c, &d);
-	number_of_arguments_for_two(res3);
-	if (possibility(a, b, c, d, r)) puts("Yes");
-	else puts("No");
+	r = get_value();
+	puts("Enter a: ");
+	a = get_value();
+	puts("Enter b: ");
+	b = get_value();
+	puts("Enter c: ");
+	c = get_value();
+	puts("Enter d: ");
+	d = get_value();
+	if (possibility(a, b, c, d, r))
+	{
+		puts("Yes");
+	}
+	else
+	{
+		puts("No");
+	}
 	return 0;
 }
 
@@ -48,27 +63,27 @@ bool possibility(double a, double b, double c, double d, double r)
 	double y = fmax(c, d);
 	double z = fmin(a, b);
 	double w = fmin(c, d);
-	double h1 = sqrt(pow(r, 2) - pow(x, 2) / 4);
-	double h2 = sqrt(pow(r, 2) - pow(y, 2) / 4);
+	double h1 = get_h(r, x);
+	double h2 = get_h(r, y);
 	double h_summ = h1 + h2;
 	double other_sides = z + w;
 	return (h_summ >= other_sides);
 }
 
-void number_of_arguments_for_two(int res)
+double get_value()
 {
-	if (res != 2)
-	{
-		puts("Error! ");
-		abort();
-	}
-}
-
-void number_of_arguments_for_one(int res)
-{
+	double a;
+	int res = scanf_s("%lf", &a);
 	if (res != 1)
 	{
-		puts("Error! ");
+		errno = EIO;
+		perror("Wrong value");
 		abort();
 	}
+	return a;
+}
+
+double get_h(double r, double side)
+{
+	return sqrt(pow(r, 2) - pow(side, 2) / 4);
 }
