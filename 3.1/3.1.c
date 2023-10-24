7803ef1affb66360680a22074f2409e6ce87a638
NebovarenkovStepan <nebovarenkovstipan@gmail.com>
2023-10-24 23:40:40 +0300

    3.1 исправление

--- 3.1/3.1.c
+++ 3.1/3.1.c
@@ -3,46 +3,48 @@
 #include <math.h>
 #include <stdbool.h>
 #include <errno.h>
-
+#include <float.h>
 /**
  * @brief Функция проверяющая существование функции в этой точке.
  * @param x сама точка.
  * @return Возвращает 1 - true или 0 - false
  */
-bool answer_possibility(float x);
+bool answer_possibility(double x);
 
 /**
- * @brief Функция принимающая и проверяющая значение на ввод.
- * @return Значение.
- */
-double get_value();
+* @brief Функция принимающая и проверяющая значение на ввод.
+* @param message - текст сообщения для пользователя.
+* @return Значение.
+*/
+double get_value(const char* message);
 
 int main()
 {
-    int res;
-    float x, dx;
-    puts("Enter x: ");
-    x = get_value();
-    puts("Enter dx: ");
-    dx = get_value();
-    do {
-        if (answer_possibility(x) == 0) {
-            printf("x=%f\n", x);
+    const double x_start = get_value("Start for x: ");
+    double x = x_start;
+    const double x_finish = get_value("Finish for x: ");
+    const double dx = get_value("Step for x: ");
+    while ((x - x_finish) <= DBL_MIN)
+    {
+        if (answer_possibility(x) == 0) 
+        {
+            printf("x=%f ", x);
             printf("%s\n", "No answer");
         }
-        x = x + dx;
-    } while (x <= 3);
+        x += dx;
+    }
     return 0;
 }
 
-bool answer_possibility(float x)
+bool answer_possibility(double x)
 {
     return ((3 * x) - 14 + exp(x) - exp(-x));
 }
 
-double get_value()
+double get_value(const char* message)
 {
     double a;
+    printf("%s", message);
     int res = scanf_s("%lf", &a);
     if (res != 1)
     {
