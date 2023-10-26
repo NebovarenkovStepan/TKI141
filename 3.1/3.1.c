ed83bbbb76d3cee4aff943dfb73aa5aded80f917
NebovarenkovStepan <nebovarenkovstipan@gmail.com>
2023-10-26 16:13:22 +0300

    Программа на костылях

--- 3.1/3.1.c
+++ 3.1/3.1.c
@@ -6,10 +6,10 @@
 #include <float.h>
 /**
  * @brief Функция проверяющая существование функции в этой точке.
- * @param x сама точка.
- * @return Возвращает 1 - true или 0 - false
+ * @param x Входной параметр - точка x.
+ * @return Возвращает результат.
  */
-bool answer_possibility(double x);
+double result(double x);
 
 /**
 * @brief Функция принимающая и проверяющая значение на ввод.
@@ -26,17 +26,14 @@ int main()
     const double dx = get_value("Step for x: ");
     while ((x - x_finish) <= DBL_MIN)
     {
-        if (answer_possibility(x) == 0) 
-        {
-            printf("x=%f ", x);
-            printf("%s\n", "No answer");
-        }
+        double y = result(x);
+        printf("%0.2lf\n", y);
         x += dx;
     }
     return 0;
 }
 
-bool answer_possibility(double x)
+double result(double x)
 {
     return ((3 * x) - 14 + exp(x) - exp(-x));
 }
