9d985ca087265c1e81d11819ba5d9cfe8932cbc6
NebovarenkovStepan <nebovarenkovstipan@gmail.com>
2023-11-02 20:01:19 +0300

    4.2 beginning

---
+++ 4.2/4.2.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <math.h>
+#include <float.h>
+#include <errno.h>
+#include <time.h>
+
+/**
+* @brief ‘ункци€ заполн€юща€ массив.
+* @param size - размер массива.
+* @param array - массив.
+* @return 1 если все хорошо.
+*/
+int fill_array(size_t size, int* array);
+
+/**
+* @brief ‘ункци€ заполн€юща€ массив рандомными числами.
+* @param size - размер массива.
+* @param array - массив.
+* @return 1 если все хорошо.
+*/
+int fill_random(size_t size, int* array);
+
