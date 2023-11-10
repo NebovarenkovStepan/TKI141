5948b63cf38533a580e0a67403df709209b3cdb1
NebovarenkovStepan <nebovarenkovstipan@gmail.com>
2023-11-10 15:02:55 +0300

    4.3

--- 4.3/4.3.c
+++ 4.3/4.3.c
@@ -12,49 +12,67 @@ enum random_or_keybord
 };
 
 /**
+* @brief Функция проверяющая ввденное значение.
+* @param value - значение которое проверяем.
+*/
+void check(value);
+
+/**
+* @brief Функция, находящая максимальный элемент массива по модулю .
+* @param array - массив.
+* @param lines - количество строк.
+* @param columns - количество столбцов.
+* @return максимальный элемент по модулю. 
+*/
+int find_max_element(int** array, size_t lines, size_t columns);
+
+/**
+* @brief Функция принимающая и проверяющая значение на ввод.
+* @param message - текст сообщение для пользователя.
+* @return Значение.
+*/
+int get_value(const char* message);
+
+/**
 * @brief Функция, выводящая массив без столбцов, которые начинаются с четного элемента.
 * @param array - массив.
-* @param wide - ширина массива.
-* @param высота - ширина массива.
-* @return 1 если все хорошо.
+* @param lines - количество строк.
+* @param columns - количество столбцов.
 */
-int print_array_third_point(int** array, size_t wide, size_t height);
+void print_array_third_point(int** array, size_t lines, size_t columns);
 
 /**
 * @brief Функция, заменяющая первые четные элементы каждого столбца в массива максимальным по модулю .
 * @param array - массив.
-* @param wide - ширина массива.
-* @param высота - ширина массива.
-* @return 1 если все хорошо.
+* @param lines - количество строк.
+* @param columns - количество столбцов.
+* @param max_element - максимальный элемент массива.
 */
-int change(int** array, size_t wide, size_t hight);
+void change(int** array, size_t lines, size_t columns, int max_element);
 
 /**
 * @brief Функция, заполняющая массив.
 * @param array - массив.
-* @param wide - ширина массива.
-* @param высота - ширина массива.
-* @return 1 если все хорошо.
+* @param lines - количество строк.
+* @param columns - количество столбцов.
 */
-int fill_array(int** array, const size_t wide, const size_t height);
+void fill_array(int** array, const size_t lines, const size_t columns);
 
 /**
 * @brief Функция, заполняющая массив рандомными числами.
 * @param array - массив.
-* @param wide - ширина массива.
-* @param высота - ширина массива.
-* @return 1 если все хорошо.
+* @param lines - количество строк.
+* @param columns - количество столбцов.
 */
-int fill_random(int** array, const size_t wide, const size_t hight);
+void fill_random(int** array, const size_t lines, const size_t columns);
 
 /**
 * @brief Функция, выводящая массив на экран.
 * @param array - массив.
-* @param wide - ширина массива.
-* @param высота - ширина массива.
-* @return 1 если все хорошо.
+* @param lines - количество строк.
+* @param columns - количество столбцов.
 */
-int print_array(int** array, size_t wide, size_t height);
+void print_array(int** array, size_t lines, size_t columns);
 
 /**
  * @brief Функция, показывающая какое значение надо ввести для рандома или ввода с клаиатуры.
@@ -68,20 +86,10 @@ void names_of_random_and_keyboard();
 int main()
 {
 	int int_wide = get_value("Enter array wide: ");
-	if (int_wide <= 0)
-	{
-		errno = EIO;
-		perror("Wrong value");
-		return 1;
-	}
+	check(int_wide);
 	size_t wide = (size_t)(int_wide);
 	int int_height = get_value("Enter array height: ");
-	if (int_height <= 0)
-	{
-		errno = EIO;
-		perror("Wrong value");
-		return 1;
-	}
+	check(int_height);
 	size_t height = (size_t)(int_height);
 	int** array = (int**)malloc(sizeof(int*) * wide);
 	for (int i = 0; i < wide; i++)
@@ -96,41 +104,36 @@ int main()
 	case Keyboard:
 	{
 		fill_array(array, wide, height);
-		print_array(array, wide, height);
-		puts("------");
-		change(array, wide, height);
-		print_array(array, wide, height);
-		puts("------");
-		print_array_third_point(array, wide, height);
 		break;
 	}
 	case Random:
 	{
 		fill_random(array, wide, height);
-		print_array(array, wide, height);
-		puts("------");
-		change(array, wide, height);
-		print_array(array, wide, height);
-		puts("------");
-		print_array_third_point(array, wide, height);
 		break;
 	}
+	print_array(array, wide, height);
+	puts("------");
+	int max_element = find_max_element(array, wide, height);
+	change(array, wide, height, max_element);
+	print_array(array, wide, height);
+	puts("------");
+	print_array_third_point(array, wide, height);
 	free(array);
 	return 0;
 	}
 }
 int get_value(const char* message)
 {
-	int a;
+	int value;
 	printf("%s", message);
-	int res = scanf_s("%d", &a);
+	int res = scanf_s("%d", &value);
 	if (res != 1)
 	{
 		errno = EIO;
 		perror("Wrong value");
 		abort();
 	}
-	return a;
+	return value;
 }
 
 void names_of_random_and_keyboard()
@@ -139,65 +142,54 @@ void names_of_random_and_keyboard()
 	printf("Random - %d\n", (int)Random);
 }
 
-int print_array(int** array,  size_t wide, size_t height)
+void print_array(int** array,  size_t lines, size_t columns)
 {
-	if (wide != height)
+	if (lines != columns)
 	{
-		int filler = wide;
-		wide = height;
-		height = filler;
+		int filler = lines;
+		lines = columns;
+		columns = filler;
 	}
-	for (size_t c = 0; c < wide; c++)
+	for (size_t c = 0; c < lines; c++)
 	{
-		for (size_t i = 0; i < height; i++)
+		for (size_t i = 0; i < columns; i++)
 		{
 			printf(" %d", array[i][c]);
 		}
 		printf("\n");
 	}
-	return 1;
 }
 
-int fill_array(int** array, const size_t wide, const size_t height)
+void fill_array(int** array, const size_t lines, const size_t columns)
 {
-	for (size_t i = 0; i < wide; i++)
+	for (size_t i = 0; i < lines; i++)
 	{
-		for (size_t c = 0; c < height; c++)
+		for (size_t c = 0; c < columns; c++)
 		{
 			array[i][c] = get_value("Enter element: ");
 		}
 	}
-	return 1;
 }
 
-int fill_random(int** array, const size_t wide, const size_t height)
+void fill_random(int** array, const size_t lines, const size_t columns)
 {
 	unsigned int ttime = (unsigned int)(time(NULL));
 	srand(ttime);
-	for (size_t i = 0; i < wide; i++)
+	for (size_t i = 0; i < lines; i++)
 	{
-		for (size_t c = 0; c < height; c++)
+		for (size_t c = 0; c < columns; c++)
 		{
 			array[i][c] = rand();
 		}
 	}
-	return 1;
 }
 
-int change(int** array, size_t wide, size_t height)
+void change(int** array, size_t lines, size_t columns, int max_element)
 {
-	int max_element = 0;
-	for (size_t i = 0; i < wide; i++)
-	{
-		for (size_t c = 0; c < height; c++)
-		{
-			max_element = max(max_element, abs(array[i][c]));
-		}
-	}
-	for (size_t i = 0; i < wide; i++)
+	for (size_t i = 0; i < lines; i++)
 	{
 		int counter = -1;
-		for (size_t c = 0; c < height; c++)
+		for (size_t c = 0; c < columns; c++)
 		{
 			if (array[i][c] % 2 == 0 && counter != i)
 			{
@@ -206,20 +198,19 @@ int change(int** array, size_t wide, size_t height)
 			}
 		}
 	}
-	return 1;
 }
 
-int print_array_third_point(int** array, size_t wide, size_t height)
+void print_array_third_point(int** array, size_t lines, size_t columns)
 {
-	if (wide != height)
+	if (lines != columns)
 	{
-		int filler = wide;
-		wide = height;
-		height = filler;
+		int filler = lines;
+		lines = columns;
+		columns = filler;
 	}
-	for (size_t c = 0; c < wide; c++)
+	for (size_t c = 0; c < lines; c++)
 	{
-		for (size_t i = 0; i < height; i++)
+		for (size_t i = 0; i < columns; i++)
 		{
 			if (array[i][0] % 2 != 0)
 			{
@@ -228,5 +219,27 @@ int print_array_third_point(int** array, size_t wide, size_t height)
 		}
 		printf("\n");
 	}
-	return 1;
+}
+
+void check(value)
+{
+	if (value <= 0)
+	{
+		errno = EIO;
+		perror("Wrong value");
+		abort();
+	}
+}
+
+int find_max_element(int** array, size_t lines, size_t columns)
+{
+	int max_element = array[0][0];
+	for (size_t i = 0; i < lines; i++)
+	{
+		for (size_t c = 0; c < columns; c++)
+		{
+			max_element = max(max_element, abs(array[i][c]));
+		}
+	}
+	return max_element;
 }
