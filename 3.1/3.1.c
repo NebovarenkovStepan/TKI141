e07d801649d18df3940c9185c0d1163c78cb0095
NebovarenkovStepan <144306213+NebovarenkovStepan@users.noreply.github.com>
2023-11-14 12:07:15 +0300

    Update 3.1.c

--- 3.1/3.1.c
+++ 3.1/3.1.c
@@ -1,4 +1,4 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <stdbool.h>
@@ -22,9 +22,15 @@ int main()
 {
     const double x_start = get_value("Start for x: ");
     double x = x_start;
-    const double x_finish = get_value("Finish for x: ");
     const double dx = get_value("Step for x: ");
-    while ((x - x_finish) <= DBL_MIN)
+    if (dx < 0)
+    {
+        errno = EIO;
+        perror("Wrong value");
+        abort();
+    }
+    const double x_finish = dx + get_value("Finish for x: ");
+    while ((x - x_finish) < DBL_EPSILON)
     {
         double y = result(x);
         printf("%lf\t%0.2lf\t\n", x, y);
